b4e0eb538ff8a21bb2b5b47ec331ff4926ab9eba
kevin.o@projectivestaffing.com <kevin.o@projectivestaffing.com>
2025-10-21 09:29:56 -0500

    add new front origin to cors white list

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(OpenaiModule);
 
   app.enableCors({
-  origin: ['http://localhost:3000', 'https://ai-assistant-one-liard.vercel.app/'], // Orígenes permitidos
+  origin: ['http://localhost:3000', 'https://ai-assistant-6yceqcyen-projective-staffings-projects.vercel.app'], // Orígenes permitidos
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
   credentials: false, // Si manejas cookies o autenticación basada en encabezados
 });
